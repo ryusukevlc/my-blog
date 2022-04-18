679bedd40bb80e4719bb950b842dcd9634c0ee14
ryusuke14520 <ryusuke14520@gmail.com>
2022-04-18 23:38:21 +0900

    [add] テストクラスの枠組みのみ作成。
    掲題通り。実装は未実施。

---
+++ src/test/java/model/article/PostArticleLogicTest.java
@@ -0,0 +1,28 @@
+package model.article;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PostArticleLogicTest extends PostArticleLogic {
+
+	@Test
+	@DisplayName("記事を新規作成する")
+	void testAddArticle() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+	@Test
+	@DisplayName("記事を削除する")
+	void testDeleteArticle() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+	@Test
+	@DisplayName("記事を更新する")
+	void testUpdateArticle() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+}
