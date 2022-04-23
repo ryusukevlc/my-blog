1f3dc9b943eda4c63cd3a0a1726f078e1b55864a
ryusuke14520 <ryusuke14520@gmail.com>
2022-04-23 19:22:36 +0900

    [add] MockとMockのインジェクション先のインスタンス化。
    掲題通り。

--- src/test/java/model/article/PostArticleLogicTest.java
+++ src/test/java/model/article/PostArticleLogicTest.java
@@ -2,16 +2,48 @@ package model.article;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-class PostArticleLogicTest extends PostArticleLogic {
+import dao.ArticlesDAO;
 
-	@Test
-	@DisplayName("記事を新規作成する")
-	void testAddArticle() {
-		fail("まだ実装されていません"); // TODO
+class PostArticleLogicTest {
+	
+	Article article;
+	
+	@Mock
+	private ArticlesDAO articleDAO = new ArticlesDAO();
+	
+	@InjectMocks
+	private PostArticleLogic postArticleLogic = new PostArticleLogic();
+	
+	@BeforeEach
+	void setUp() {
+		
+		MockitoAnnotations.initMocks(this);
+		
+		article = new Article();
+		article.setTitle("testTitle");
+		article.setContent("testContent");
+		
+		
 	}
+		
+	@Test
+	@DisplayName("引数を渡す") 
+	void testAddArticle1(Article article) {
+		
+	}
+	
+	@Test
+	void testAddArticle2() {
+		
+	}
+	
 
 	@Test
 	@DisplayName("記事を削除する")
