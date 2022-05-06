dcb9327ace6aadb878c91f9d785623aa3a2b116b
ryusuke14520 <ryusuke14520@gmail.com>
2022-05-06 23:25:53 +0900

    [add] htmlインジェクション対策のため、タグを実体参照に変換する処理を追加。
    掲題通り。今回追加したのは＜（小なり）と＞（大なり）についてのみ。

--- src/main/java/validator/ArticleValidator.java
+++ src/main/java/validator/ArticleValidator.java
@@ -24,6 +24,44 @@ public class ArticleValidator {
             errors.add("内容が入力されていません。");
         }
         
+        //htmlインジェクション対策（title）
+        String changedTitle = "";
+        //htmlタグを無効化 "<" 実体参照に変換したうえでDBに登録する。
+        if (title.matches(".*<.*")) {
+        	changedTitle = title.replaceAll("(?<=.*)<(?=.*)", "&lt;");
+        }
+        
+        //htmlタグを無効化 ">"　実体参照に変換したうえでDBに登録する。
+        if (title.matches(".*>.*") && changedTitle.isEmpty()) {
+        	changedTitle = title.replaceAll("(?<=.*)>(?=.*)", "&gt;");
+        } else if (title.matches(".*>.*")) {
+        	changedTitle = changedTitle.replaceAll("(?<=.*)>(?=.*)", "&gt;");
+        }
+        
+        //htmlインジェクション対策（content）
+        String changedContent = "";
+        //htmlタグを無効か　"<" 実体参照に変換したうえでDBに登録する。
+        if (content.matches(".*<.*")) {
+        	changedContent = content.replaceAll("(?<=.*)<(?=.*)", "&lt;");
+        }
+        
+        //htmlタグを無効化　">" 実体参照に変換したうえでDBに登録する。
+        if (content.matches(".*>.*") && changedContent.isEmpty()) {
+        	changedContent = content.replaceAll("(?<=.*)>(?=.*)", "&gt;");
+        } else if (content.matches(".*>.*")) {
+        	changedContent = changedContent.replaceAll("(?<=.*)>(?=.*)", "&gt;");
+        }
+        
+        //実体参照に変換したテキストをarticleに格納する（title）
+        if (!(changedTitle.isEmpty())) {
+        	article.setTitle(changedTitle);
+        }
+        
+      //実体参照に変換したテキストをarticleに格納する（content）
+        if(!(changedContent.isEmpty())) {
+        	article.setContent(changedContent);
+        }
+        
         return errors;
         
     }
