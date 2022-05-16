492fbcfef72f90afd9b17c47779def4e1ee7bc4b
ARATA\ryusu <ryusuke14520@gmail.com>
2022-05-17 00:29:39 +0900

    [add] webAPIに関する処理を記述するクラスを作成。
    掲題通り。

---
+++ src/main/java/model/webAPI/WebAPIProcessing.java
@@ -0,0 +1,20 @@
+package model.webAPI;
+
+/**
+ * WebAPIProcessing<br>
+ * webAPIに関する処理
+ * 
+ *
+ */
+public class WebAPIProcessing {
+	
+	/**
+	 * getWeatherInTokyo<br>
+	 * 東京の天気を取得する
+	 * 
+	 */
+	public void getWeatherInTokyo() {
+		
+	}
+
+}
