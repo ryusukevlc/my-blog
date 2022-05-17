84b6ef7a1d5ff896543ccfed7dca20225d7ee58a
ARATA\ryusu <ryusuke14520@gmail.com>
2022-05-18 00:30:52 +0900

    [add] 東京の天気を取得する処理を追加。
    掲題通り。

--- src/main/java/model/webAPI/WebAPIProcessing.java
+++ src/main/java/model/webAPI/WebAPIProcessing.java
@@ -1,5 +1,13 @@
 package model.webAPI;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * WebAPIProcessing<br>
  * webAPIに関する処理
@@ -7,14 +15,58 @@ package model.webAPI;
  *
  */
 public class WebAPIProcessing {
-	
+
 	/**
 	 * getWeatherInTokyo<br>
 	 * 東京の天気を取得する
 	 * 
 	 */
-	public void getWeatherInTokyo() {
-		
+	public String getWeatherInTokyo() {
+
+		// webAPIのURL
+		String openMeteoUrl = "https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=weathercode&timezone=Asia%2FTokyo";
+
+		// 結果を格納する変数
+		String result = "";
+		HttpURLConnection con = null;
+		BufferedReader in = null;
+
+		try {
+			URL url = new URL(openMeteoUrl);
+			con = (HttpURLConnection) url.openConnection();
+			con.connect();
+
+			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+			String tmp = "";
+
+			while ((tmp = in.readLine()) != null) {
+				result += tmp;
+			}
+
+			con.disconnect();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			try {
+				in.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			con.disconnect();
+		}
+
+		JSONObject json = new JSONObject(result);
+
+		JSONObject hourly = json.getJSONObject("hourly");
+		JSONArray weatherCode = hourly.getJSONArray("weathercode");
+		String weatherCodeNow = weatherCode.getString(0);
+
+		return weatherCodeNow;
+
 	}
 
 }
